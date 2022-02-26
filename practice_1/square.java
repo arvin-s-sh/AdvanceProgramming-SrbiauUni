e47dc64a93e4c4679d8893204ef65b0f6e28a8f8
arvin-s-sh <99652662+arvin-s-sh@users.noreply.github.com>
2022-02-26 14:07:52 +0330

    Add files via upload

---
+++ practice_1/square.java
@@ -0,0 +1,12 @@
+package practice_1;
+
+public class square {
+    float lenth;
+    float hight;
+    public square(float Lenth, float Hight){
+        this.lenth = Lenth;
+        this.hight = Hight;
+
+    }
+
+}
