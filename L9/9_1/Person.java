31f3d7de94f85214a2aa8dbc76ba1044e69eeaf9
arvin-s-sh <arvin.s.sh@gmail.com>
2022-05-13 01:11:28 +0430

    java

---
+++ L9/9_1/Person.java
@@ -0,0 +1,17 @@
+package L9_1;
+
+public class Person {
+    int id ;
+    String name;
+    int age;
+    String email;
+    String job;
+
+    public Person(int id, String name, int age, String email, String job) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.email = email;
+        this.job = job;
+    }
+}
